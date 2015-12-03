ab76f94c1547c45c57736aa6a44e54e3de11e839
jskaroff <jskaroff@anthropologie.com>
2015-12-03 09:45:58 -0500

    Instantiating obojects

--- iOSTraining/Computer.m
+++ iOSTraining/Computer.m
@@ -25,7 +25,6 @@
     self.hasUsb = NO;
     self.hasWifi = NO;
     
-    
 }
 
 @end
--- iOSTraining/ViewController.m
+++ iOSTraining/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import "iPhone.h"
 
 @interface ViewController ()
 
@@ -17,6 +18,24 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    Computer *c1 = [[Computer alloc] init];
+    c1.make = @"Commodore";
+    c1.model = @"64";
+    
+    iPhone *i1 = [[iPhone alloc] init];
+    i1.make = @"Apple";
+    i1.model = @"iPhone 6s+";
+    i1.megabytesOfStorage = 128 * 1024;
+    
+    iPhone *i2 = [[iPhone alloc] init];
+    [i2 setupComputerValues];
+    i1.make = @"Apple";
+    i1.model = @"iPhone 4";
+    i1.megabytesOfStorage = 16 * 1024;
+    
+    NSLog(@"i1 Values = %@ - %@ - %@", i1, i1.make, i1.model);
+    NSLog(@"i2 Values = %@ - %@ - %@", i2, i2.make, i2.model);
 }
 
 - (void)didReceiveMemoryWarning {
